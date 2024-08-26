ddc99262f5b6a84f33c7fdc3054795bf6bbabb8d
João Victor <jvpcms.engenharia@gmail.com>
2024-08-25 21:13:23 -0300

    load .env

--- api/src/main/java/com/literario/api/ApiApplication.java
+++ api/src/main/java/com/literario/api/ApiApplication.java
@@ -3,10 +3,19 @@ package com.literario.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class ApiApplication {
 
 	public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.load();
+
+		System.setProperty("POSTGRES_URL", dotenv.get("POSTGRES_URL"));
+        System.setProperty("POSTGRES_USER", dotenv.get("POSTGRES_USER"));
+        System.setProperty("POSTGRES_PASSWORD", dotenv.get("POSTGRES_PASSWORD"));
+
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
