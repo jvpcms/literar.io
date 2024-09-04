1f80456874fa2a93f2062de2e9dcd26ae45bc248
João Victor <jvpcms.engenharia@gmail.com>
2024-09-04 12:43:59 -0300

    remove squema.slq, handle table creation with hibernate

--- api/src/main/java/com/literario/api/repo/ReviewRepo.java
+++ api/src/main/java/com/literario/api/repo/ReviewRepo.java
@@ -9,31 +9,31 @@ import org.springframework.data.jpa.repository.Modifying;
 import java.util.List;
 import java.util.UUID;
 
-import com.literario.api.model.Review;
+import com.literario.api.model.ReviewEntity;
 
 import jakarta.transaction.Transactional;
 
 @RepositoryRestResource
-public interface ReviewRepo extends JpaRepository<Review,UUID> {
+public interface ReviewRepo extends JpaRepository<ReviewEntity,UUID> {
     
-    @Query("SELECT re FROM Review re WHERE re.bookId = :bookId")
-    List<Review> findReviewsByBook(@Param("bookId") UUID bookId);
+    @Query("SELECT re FROM ReviewEntity re WHERE re.book = :bookId")
+    List<ReviewEntity> findReviewsByBook(@Param("bookId") UUID bookId);
     
-    @Query("SELECT re FROM Review re WHERE re.userId = :userId")
-    List<Review> findReviewsByUser(@Param("userId") UUID userId);
+    @Query("SELECT re FROM ReviewEntity re WHERE re.user = :userId")
+    List<ReviewEntity> findReviewsByUser(@Param("userId") UUID userId);
 
     @Transactional
     @Modifying
-    @Query("INSERT INTO Review (userId, bookId, rating, review) VALUES (:userId, :bookId, :rating, :review)")
-    void insertReview(@Param("userId") UUID userId, @Param("bookId") UUID bookId, @Param("rating") float rating, @Param("review") String review);
+    @Query("INSERT INTO ReviewEntity (user, book, rating, review) VALUES (:userId, :bookId, :rating, :review)")
+    void insertReview(@Param("userId") UUID userId, @Param("bookId") UUID bookId, @Param("rating") Integer rating, @Param("review") String review);
 
     @Transactional
     @Modifying
-    @Query("UPDATE Review SET rating = :rating WHERE id = :reviewId")
+    @Query("UPDATE ReviewEntity SET rating = :rating WHERE id = :reviewId")
     void updateReview(@Param("reviewId") UUID reviewId, @Param("rating") Integer rating);
 
     @Transactional
     @Modifying
-    @Query("DELETE FROM Review WHERE id = :reviewId")
+    @Query("DELETE FROM ReviewEntity WHERE id = :reviewId")
     void deleteReview(@Param("reviewId") UUID reviewId);
 }
