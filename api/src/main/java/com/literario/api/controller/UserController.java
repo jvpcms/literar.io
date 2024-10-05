cc575add695830329d780f1df6a7e77464f30283
João Victor <jvpcms.engenharia@gmail.com>
2024-10-05 19:55:27 -0300

    merge main -> set-jwt-filter

--- api/src/main/java/com/literario/api/controller/UserController.java
+++ api/src/main/java/com/literario/api/controller/UserController.java
@@ -4,25 +4,20 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.UUID;
-import java.util.Map;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import com.literario.api.service.UserService;
-import com.literario.api.service.PasswordService;
 import com.literario.api.service.ReviewService; 
 
 import com.literario.api.model.ReviewEntity;
 
-import com.literario.api.model.NotAuthedUserEntity;
 import com.literario.api.repo.UserRepo;
 import com.literario.api.repo.ReviewRepo;
 import com.literario.api.repo.BookRepo;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.ResponseEntity;
 
 
 @RestController
@@ -30,14 +25,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class UserController {
 
     private UserRepo userRepo;
-    private UserService userService;
-    private PasswordService passwordService;
     private ReviewService reviewService;
 
     public UserController(UserRepo userRepo, BookRepo bookRepo, ReviewRepo reviewRepo) {
         this.userRepo = userRepo;
-        this.userService = new UserService(userRepo);
-        this.passwordService = new PasswordService();
         this.reviewService = new ReviewService(reviewRepo, bookRepo, userRepo);
     }
 
