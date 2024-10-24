efae7d2e05a277f5e91fca2b8734a33aa460a209
Gabriel Dias Vilela <bielvileladias1@gmail.com>
2024-10-24 13:51:25 -0300

    fix author controller

--- api/src/main/java/com/literario/api/repo/AuthorRepo.java
+++ api/src/main/java/com/literario/api/repo/AuthorRepo.java
@@ -5,10 +5,13 @@ import java.util.UUID;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface AuthorRepo extends JpaRepository<AuthorEntity, UUID> {
     
     default Optional<AuthorEntity> findAuthorById(UUID authorId){
-        return findById(authorId);
+        Optional<AuthorEntity> author = findById(authorId);
+        return author;
     }
 }
